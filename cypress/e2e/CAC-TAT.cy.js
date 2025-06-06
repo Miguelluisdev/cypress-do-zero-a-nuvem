b2f3b0cc49f5f5258cecbe3d21400c935f13cdf4
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-06-06 00:02:13 -0300

    feat: exercicios 1 , 2 , 3 e 4

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -11,15 +11,43 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   // only = somente faz executar apenas o teste que está com .only
   it('preenche os campos obrigatórios e envia o formulário' , () => {
-    
+    const longText = Cypress._.repeat('abcdfghifojgheifeeefegjyjy' , 10)
   cy.get('#firstName').type('Miguel').should('have.value' , 'Miguel')
   cy.get('#lastName').type('Luis').should('have.value' , 'Luis')
   cy.get('#email').type('darius@gmail.com').should('have.value' , 'darius@gmail.com')
-  cy.get('#open-text-area').type('mensagem extra').should('have.value' , 'mensagem extra')
+  cy.get('#open-text-area').type(longText , {delay: 0})
+  // .should('have.value' , 'mensagem extra')
   cy.contains('Enviar').click()
-  
+
 
   cy.contains('Mensagem enviada com sucesso.').should('be.visible', 'Mensagem enviada com sucesso.')
   })
 
+  
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida' , () => {
+   cy.get('#email').type('dariusgmail.com').should('have.value' , 'dariusgmail.com')
+   cy.contains('Enviar').click()
+  
+   cy.get('.error').should('be.visible')
+  })
+  
+  it('campo telefone continua vazio quando um valor não numerico é inserido' , () => {
+    cy.get('#phone')
+    .type('abdce')
+    .should('have.value' , '')
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário' , () => {
+        const longText = Cypress._.repeat('abcdfghifojgheifeeefegjyjy' , 10)
+  cy.get('#firstName').type('Miguel').should('have.value' , 'Miguel')
+  cy.get('#lastName').type('Luis').should('have.value' , 'Luis')
+  cy.get('#email').type('darius@gmail.com').should('have.value' , 'darius@gmail.com')
+  cy.get('#open-text-area').type(longText , {delay: 0})
+  // .should('have.value' , 'mensagem extra')
+  cy.get('#phone-checkbox').click()
+  cy.get('button[type="submit"]').click()
+
+
+ cy.get('.error').should('be.visible')
+  })
 })
