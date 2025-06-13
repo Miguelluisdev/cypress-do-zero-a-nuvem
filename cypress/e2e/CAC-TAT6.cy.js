90eeb078259040c84884477b021f2d5d22aa6ebf
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-06-13 16:39:53 -0300

    feat: sessão 7 e 8 finalizadas e começando a 9

--- cypress/e2e/CAC-TAT6.cy.js
+++ cypress/e2e/CAC-TAT6.cy.js
@@ -1,7 +1,30 @@
-describe('Lidando com links que abrem em outra aba' ,() => {
+describe("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html");
+  });
 
-   it('' , () => {
+  it("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", () => {
+    cy.contains("a", "Política de Privacidade")
+      .should("have.attr", "href", "privacy.html")
+      .and("have.attr", "target", "_blank");
+  });
 
-   })
-   
-})
+  it("acessa a página da política de privacidade removendo o target e então clicando no link", () => {
+    cy.contains("a", "Política de Privacidade")
+      .should("have.attr", "target", "_blank")
+      .invoke("removeAttr", "target")
+      .click();
+
+    cy.contains("h1", "CAC TAT - Política de Privacidade").should("be.visible");
+  });
+
+    it("testa a página da política de privacidade de forma independente", () => {
+    cy.contains("a", "Política de Privacidade")
+      .should("have.attr", "target", "_blank")
+      .invoke("removeAttr", "target")
+      .click();
+
+    cy.contains("h1", "CAC TAT - Política de Privacidade").should("be.visible");
+  });
+
+});
---
+++ cypress/e2e/CAC-TAT7.cy.js
@@ -0,0 +1,12 @@
+describe("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html");
+  });
+
+
+  it('teste' , () => {
+    
+  })
+  
+
+});
