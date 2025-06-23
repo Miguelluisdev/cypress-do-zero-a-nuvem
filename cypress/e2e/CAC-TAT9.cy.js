ff6480df8ee9e16c5dfaafa9fff4ff394c1f98d8
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-06-23 16:32:06 -0300

    feat: aprendendo o cu.request

---
+++ cypress/e2e/CAC-TAT9.cy.js
@@ -0,0 +1,21 @@
+describe("exibe mensagem por 3 segundos", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html");
+  });
+
+  it("faz uma requisição HTTP", () => {
+    cy.request(
+      "GET",
+      "https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html"
+    )
+      .as("getRequest")
+      .its("status")
+      .should("be.equal", 200);
+
+    cy.get("@getRequest").its("statusText")
+        .should('be.equal' , 'OK')
+        
+    cy.get("@getRequest").its("body")
+        .should('include' , 'CAC TAT')
+  });
+});
